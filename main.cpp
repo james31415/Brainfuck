bc8ce399a15a7669e4d4a6b1dfb52c6c70a15a12
James Hall <james31415@gmail.com>
2018-04-20 22:23:38 -0400

    First

--- main.cpp
+++ main.cpp
@@ -8,48 +8,42 @@ typedef unsigned char BYTE;
 
 typedef std::vector<BYTE> Internal;
 
-class Storage
+struct Storage
 {
-    public:
-        Storage();
-        Storage& operator++();
-        Storage& operator--();
-        BYTE& operator*();
-        const BYTE& operator*() const;
-        friend std::ostream& operator<<(std::ostream& os, const Storage& sto);
+    friend std::ostream& operator<<(std::ostream& os, const Storage& sto);
 
-    private:
-        unsigned int ptr;
-        Internal m_vStore;
+    unsigned int ptr;
+    Internal m_vStore;
 };
 
-Storage::Storage() {
-    ptr = 0;
-    m_vStore.push_back(0);
+void
+StorageConstruct(Storage* pThis) {
+    pThis->ptr = 0;
+    pThis->m_vStore.push_back(0);
 }
 
-Storage& Storage::operator++() {
-    if (ptr + 1 == m_vStore.size())
-        m_vStore.push_back(0);
-    ++ptr;
+Storage& StorageIncrement(Storage* pThis)
+{
+    if (pThis->ptr + 1 == pThis->m_vStore.size())
+        pThis->m_vStore.push_back(0);
+    ++pThis->ptr;
 
-    return *this;
+    return *pThis;
 }
 
-Storage& Storage::operator--() {
-    if (ptr == 0)
+Storage& StorageDecrement(Storage* pThis)
+{
+    if (pThis->ptr == 0)
         throw "Sorry";
-    --ptr;
+    --pThis->ptr;
 
-    return *this;
+    return *pThis;
 }
 
-BYTE& Storage::operator*() {
-    return m_vStore[ptr];
-}
-
-const BYTE& Storage::operator*() const {
-    return m_vStore[ptr];
+BYTE&
+StorageFetch(Storage* pThis)
+{
+    return pThis->m_vStore[pThis->ptr];
 }
 
 std::ostream& operator<<(std::ostream& os, const Storage& sto) {
@@ -58,110 +52,73 @@ std::ostream& operator<<(std::ostream& os, const Storage& sto) {
     return os;
 }
 
-class Brainfuck
+struct Brainfuck
 {
-    public:
-        Brainfuck();
-        Brainfuck(const std::string& str);
-        void SetString(const std::string& str);
-        void Go();
-
-    private:
-        void GT();
-        void LT();
-        void PL();
-        void MI();
-        void DT();
-        void CM();
-        void OB();
-        void CB();
-        unsigned int ip;
-        std::string m_str;
-        Storage sto;
+    unsigned int ip;
+    std::string m_str;
+    Storage sto;
 };
 
-Brainfuck::Brainfuck() : ip(0) { std::cin >> std::noskipws; }
-
-Brainfuck::Brainfuck(const std::string& str) : ip(0), m_str(str) {
+void
+BrainfuckConstruct(Brainfuck* pThis)
+{
+    pThis->ip = 0;
+    StorageConstruct(&pThis->sto);
+    std::cin >> std::noskipws;
 }
 
-void Brainfuck::SetString(const std::string& str) { m_str = str; }
-
-void Brainfuck::Go() {
+void
+Go(Brainfuck* pThis) {
 #if _DEBUG
     int i = 0;
 #endif
-    while (ip != m_str.length()) {
-        const char c = m_str[ip];
+    while (pThis->ip != pThis->m_str.length()) {
+        const char c = pThis->m_str[pThis->ip];
 #if _DEBUG
-        std::cout << ++i << ": " << c << ": " << sto << std::endl;
+        std::cout << ++i << ": " << c << ": " << pThis->sto << std::endl;
 #endif
         switch (c) {
-            case '>': GT(); break;
-            case '<': LT(); break;
-            case '+': PL(); break;
-            case '-': MI(); break;
-            case '.': DT(); break;
-            case ',': CM(); break;
-            case '[': OB(); break;
-            case ']': CB(); break;
+            case '>': StorageIncrement(&pThis->sto); break;
+            case '<': StorageDecrement(&pThis->sto); break;
+            case '+': ++StorageFetch(&pThis->sto); break;
+            case '-': --StorageFetch(&pThis->sto); break;
+            case '.': std::cout << StorageFetch(&pThis->sto); break;
+            case ',': std::cin >> StorageFetch(&pThis->sto); break;
+            case '[': 
+            {
+                if (StorageFetch(&pThis->sto) == 0)
+                {
+                    int iDepth = 1;
+                    while (iDepth > 0)
+                    {
+                        ++pThis->ip;
+                        switch (pThis->m_str[pThis->ip]) {
+                            case '[': ++iDepth; break;
+                            case ']': --iDepth; break;
+                            default: break;
+                        };
+                    }
+                }
+            } break;
+            case ']':
+            {
+                if (StorageFetch(&pThis->sto) != 0)
+                {
+                    int iDepth = 1;
+                    while (iDepth > 0) {
+                        --pThis->ip;
+                        switch (pThis->m_str[pThis->ip])
+                        {
+                            case '[': --iDepth; break;
+                            case ']': ++iDepth; break;
+                            default: break;
+                        };
+                    }
+                }
+            } break;
             default: break;
         };
-        ++ip;
-    }
-}
-
-void Brainfuck::GT() {
-    ++sto;
-}
-
-void Brainfuck::LT() {
-    --sto;
-}
-
-void Brainfuck::PL() {
-    ++*sto;
-}
-
-void Brainfuck::MI() {
-    --*sto;
-}
-
-void Brainfuck::DT() {
-    std::cout << *sto;
-}
-
-void Brainfuck::CM() {
-    std::cin >> *sto;
-}
-
-void Brainfuck::OB() {
-   if (*sto == 0)
-   {
-       int iDepth = 1;
-       while (iDepth > 0) {
-           ++ip;
-           switch (m_str[ip]) {
-               case '[': ++iDepth; break;
-               case ']': --iDepth; break;
-               default: break;
-           };
-       }
-   }
-}
-
-void Brainfuck::CB() {
-    if (*sto != 0)
-    {
-        int iDepth = 1;
-        while (iDepth > 0) {
-            --ip;
-            switch (m_str[ip]) {
-                case '[': --iDepth; break;
-                case ']': ++iDepth; break;
-                default: break;
-            };
-        }
+        ++pThis->ip;
     }
 }
 
@@ -197,12 +154,13 @@ std::string parseArguments(int argc, char* argv[]) {
 
 int main(int argc, char* argv[]) {
     Brainfuck bf;
+    BrainfuckConstruct(&bf);
 
     std::string str = parseArguments(argc, argv);
-    bf.SetString(str);
+    bf.m_str = str;
 
     try {
-        bf.Go();
+        Go(&bf);
     } catch (const char* s) {
         std::cout << s << std::endl;
     }
