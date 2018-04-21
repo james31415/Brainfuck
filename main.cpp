b8553f0a43179b5d7681b0ca89f3ae83246e39db
James Hall <james31415@gmail.com>
2018-04-20 22:46:17 -0400

    Blah

--- main.cpp
+++ main.cpp
@@ -1,31 +1,42 @@
 #include <cstring>
 #include <fstream>
-#include <iostream>
 #include <string>
-#include <vector>
+
+#include <cstdio>
 
 typedef unsigned char BYTE;
 
-typedef std::vector<BYTE> Internal;
-
 struct Storage
 {
-    friend std::ostream& operator<<(std::ostream& os, const Storage& sto);
-
     unsigned int ptr;
-    Internal m_vStore;
+
+    BYTE* m_vStore;
+    unsigned int size;
 };
 
 void
 StorageConstruct(Storage* pThis) {
     pThis->ptr = 0;
-    pThis->m_vStore.push_back(0);
+    pThis->size = 30000;
+    pThis->m_vStore = (BYTE*)malloc(pThis->size * sizeof(pThis->m_vStore));
+    memset(pThis->m_vStore, 0, pThis->size);
 }
 
 Storage& StorageIncrement(Storage* pThis)
 {
-    if (pThis->ptr + 1 == pThis->m_vStore.size())
-        pThis->m_vStore.push_back(0);
+    if (pThis->ptr + 1 == pThis->size)
+    {
+        pThis->size = 3 * pThis->size / 2;
+        BYTE* temp = (BYTE*)realloc(pThis->m_vStore, pThis->size);
+        if (temp)
+        {
+            pThis->m_vStore = temp;
+        }
+        else
+        {
+            throw "Very sorry";
+        }
+    }
     ++pThis->ptr;
 
     return *pThis;
@@ -46,12 +57,6 @@ StorageFetch(Storage* pThis)
     return pThis->m_vStore[pThis->ptr];
 }
 
-std::ostream& operator<<(std::ostream& os, const Storage& sto) {
-    os << "(" << sto.ptr << ") " << static_cast<unsigned int>(sto.m_vStore[sto.ptr]);
-
-    return os;
-}
-
 struct Brainfuck
 {
     unsigned int ip;
@@ -64,26 +69,19 @@ BrainfuckConstruct(Brainfuck* pThis)
 {
     pThis->ip = 0;
     StorageConstruct(&pThis->sto);
-    std::cin >> std::noskipws;
 }
 
 void
 Go(Brainfuck* pThis) {
-#if _DEBUG
-    int i = 0;
-#endif
     while (pThis->ip != pThis->m_str.length()) {
         const char c = pThis->m_str[pThis->ip];
-#if _DEBUG
-        std::cout << ++i << ": " << c << ": " << pThis->sto << std::endl;
-#endif
         switch (c) {
             case '>': StorageIncrement(&pThis->sto); break;
             case '<': StorageDecrement(&pThis->sto); break;
             case '+': ++StorageFetch(&pThis->sto); break;
             case '-': --StorageFetch(&pThis->sto); break;
-            case '.': std::cout << StorageFetch(&pThis->sto); break;
-            case ',': std::cin >> StorageFetch(&pThis->sto); break;
+            case '.': putchar(StorageFetch(&pThis->sto)); break;
+            case ',': StorageFetch(&pThis->sto) = getchar(); break;
             case '[': 
             {
                 if (StorageFetch(&pThis->sto) == 0)
@@ -141,7 +139,7 @@ std::string parseArguments(int argc, char* argv[]) {
                 readString(fs, str);
                 return str;
             } else {
-                std::cout << "Need filename. Using hello world." << std::endl;
+                printf("Need filename. Using hello world.\n");
             }
         } else {
             return argv[1];
@@ -156,13 +154,12 @@ int main(int argc, char* argv[]) {
     Brainfuck bf;
     BrainfuckConstruct(&bf);
 
-    std::string str = parseArguments(argc, argv);
-    bf.m_str = str;
+    bf.m_str = parseArguments(argc, argv);
 
     try {
         Go(&bf);
     } catch (const char* s) {
-        std::cout << s << std::endl;
+        printf("%s\n", s);
     }
 
     return 0;
